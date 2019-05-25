48f2daee9b53b9a0e6acf766453735094a009bb7
Humza Younus <humzayounus@Humzas-MacBook-Pro.local>
2019-05-25 23:23:58 +0100

    Finished startup

---
+++ src/main/java/service/AccountService.java
@@ -0,0 +1,19 @@
+package service;
+
+import domain.Account;
+import repo.AccountRepo;
+
+public class AccountService {
+	
+	private AccountRepo repo = new AccountRepo();
+	private int count;
+	
+	public Account addAccount(Account account) {
+		
+		return this.repo.getAccounts().put(count, account);
+	}
+	
+	public Account getAccount(int number) {
+		return this.repo.getAccounts().get(number);
+	}
+}
